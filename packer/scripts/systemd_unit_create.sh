02c7849c734812240f3e7a333a8dc1fc804f626f
Andrew Sokolov <andrewfalc0n@yandex.ru>
2018-04-16 18:15:55 +0300

    Homework-10 (part1)

--- packer/scripts/systemd_unit_create.sh
+++ packer/scripts/systemd_unit_create.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mv /home/appuser/puma.service /etc/systemd/system/ 
+mv /home/scouserandrew/puma.service /etc/systemd/system/ 
 
 cd ~/
 git clone -b monolith https://github.com/express42/reddit.git
 cd ./reddit
 bundle install
 
-cd /home/appuser/reddit
+cd /home/scouserandrew/reddit
 bundle binstubs puma --path ./sbin
 
 systemctl daemon-reload
